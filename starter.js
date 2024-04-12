8ab5e49e27940d4a41b0029d849dd7ebe038956b
Emil Skogsund <skogix@gmail.com>
2024-04-12 21:41:29 +0200

    .

--- starter.js
+++ starter.js
@@ -1,10 +1,14 @@
 // exempel
 // import { findServers } from '/scripts/helpers/find-servers.js';
 // import { FORMAT_MONEY, HOME, PURCHASED_SERVER_PREFIX } from '/scripts/constants.js';
+import {
+  log
+} from './lib/helpers.js'
 /** @param {NS} ns **/
 export async function main(ns) {
-
+  log(ns, "test");
 // gå till university
+  // kommer med singularity (?)
 // vid x hacking, hacka
 // jobba
 // vid var x:e level, kör en ny scan
