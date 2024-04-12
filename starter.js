0d7682f4f1c3c2e1560db7049ef97da58e098d14
Emil Skogsund <skogix@gmail.com>
2024-04-12 22:35:50 +0200

    .

--- starter.js
+++ starter.js
@@ -2,20 +2,21 @@ import {
   log
 } from 'skogix/lib/helpers.js'
 import {
-  scanAll
+  scanAll, gainRootAccess
 } from 'skogix/lib/scanner.js'
 /** @param {NS} ns **/
 export async function main(ns) {
   let test = [];
   test = scanAll(ns, "home");
-  test.forEach((server) => log(ns,server,"SUCCESS"));
+  test.forEach((server) => log(ns,server));
   let msg = "Scannade " + test.length + " servrar.";
   log(ns, msg, false, "success");
-
+  test.forEach((server) => gainRootAccess(ns,server));
+  
 // gå till university
   // kommer med singularity (?)
 // vid x hacking, hacka
 // jobba
 // vid var x:e level, kör en ny scan
 // tor-router
-
+}
