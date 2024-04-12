c17f2f824b1e760d520d68f668043bfd67d2f648
Emil Skogsund <skogix@gmail.com>
2024-04-12 22:15:30 +0200

    .

--- starter.js
+++ starter.js
@@ -1,12 +1,20 @@
 // exempel
-// import { findServers } from '/scripts/helpers/find-servers.js';
-// import { FORMAT_MONEY, HOME, PURCHASED_SERVER_PREFIX } from '/scripts/constants.js';
+// import { findServers } from 'skogix//scripts/helpers/find-servers.js';
+// import { FORMAT_MONEY, HOME, PURCHASED_SERVER_PREFIX } from 'skogix//scripts/constants.js';
 import {
   log
-} from './lib/helpers.js'
+} from 'skogix/lib/helpers.js'
+import {
+  scanAll
+} from 'skogix/lib/scanner.js'
 /** @param {NS} ns **/
 export async function main(ns) {
   log(ns, "test");
+  let test = [];
+  test = scanAll(ns, "home");
+  test.forEach((server) => log(ns,server,ToastVariant.SUCCESS));
+  let msg = "Scannade " + test.count() " servrar.";
+  log(ns, msg, "SUCCESS");
 // gå till university
   // kommer med singularity (?)
 // vid x hacking, hacka
@@ -14,3 +22,4 @@ export async function main(ns) {
 // vid var x:e level, kör en ny scan
 // tor-router
 }
+
