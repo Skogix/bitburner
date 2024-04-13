2d3b8def40474580380a50f7faf57e92b69312b8
Emil Skogsund <skogix@gmail.com>
2024-04-13 05:18:25 +0200

    .

--- SJDeploy.js
+++ SJDeploy.js
@@ -1,10 +1,18 @@
+import {
+  log
+} from 'skogix/lib/helpers.js'
+import {
+  scanAll, gainRootAccess
+} from 'skogix/lib/scanner.js'
 /** @param {NS} ns */
 export async function main(ns) {
   let target = ns.args[0]
   let listaMedAllaServrar = ns.getPurchasedServers()
+  let nyaLista = scanAll(ns, 'home')
+  listaMedAllaServrar = listaMedAllaServrar.concat(nyaLista)
   listaMedAllaServrar.forEach((server) => {
     ns.tprint(server)
     ns.scp('SJHack.js', server, 'home') 
     ns.exec('SJHack.js', server, 1, target.toString())
   })
-
+}
