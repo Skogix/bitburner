56f933bd59e8869774f35e551dc5962fbc39e450
Emil Skogsund <skogix@gmail.com>
2024-04-13 14:50:03 +0200

    .

---
+++ skogix/start.js
@@ -0,0 +1,18 @@
+import {
+  log
+} from 'skogix/lib/helpers.js'
+import {
+  scanAll, gainRootAccess
+} from 'skogix/lib/scanner.js'
+
+/** @param {NS} ns **/
+export async function main(ns) {
+alias 1='run /kill-all-scripts.js'
+alias 2='run /stockmaster.js --noisy true'
+alias 3='run /host-manager.js --run-continuously true --utilization-trigger 0.7 --reserve-percent 0.3 --interval 50000'
+alias 4='run /hacknet-upgrade-manager.js --continuous true --max-payoff-time "4h" --interval 200'
+alias 5='run /daemon.js --xp-only true --use-hacknet-servers true --initial-max-targets 10 --share true --silent-misfires true'
+alias 6='run /stats.js --show-peoplekilled true'
+alias 7='run /autoinfiltrate.js'
+
+}
